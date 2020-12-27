0ea99e2ff94c387b9cfef850039ecff1b4a194be
rahulcode751 <73958355+rahulcode751@users.noreply.github.com>
2020-12-27 14:41:56 +0530

    Add files via upload

---
+++ associativity.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int x=2,y=3;
+	//printf("the value of 8*y/3*x is %d\n", 8*y/3*x);
+	// that is awrong answer 8*3/3*2= 24/6
+	// 24/3*2
+	// 8*2
+	//
+	printf("the value of (8*)")
+	return 0;
+}
